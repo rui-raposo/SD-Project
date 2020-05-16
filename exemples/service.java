6515269c519933011b91ce96dc2ef862465aa73a
rui-raposo <ruiraposo7@gmail.com>
2020-05-16 21:27:09 +0100

    Comments

--- exemples/service.java
+++ exemples/service.java
@@ -4,10 +4,17 @@ import javax.faces.bean.ManagedBean;
 
 
 @ManagedBean(name="signIn")
+
 public class SignIn {
     
+    // Class variables
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
     private String name, password;
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
 
+
+    // Getters and Setters
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
     public String getName() {
         return name;
     }
@@ -23,47 +30,84 @@ public class SignIn {
     public void setPassword(String password) {
         this.password = password;
     }
-    
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    /*
+     * Goal            : Validate the sign in
+     * Parameters      : Username and Password inserted from the input fields (index.xhtml)
+     * Security        : Decrypt from database and compare with the inserted password ?? [To be approved]
+     * Dificulty Level : [0-10] --> 1
+     */
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
     public String signInFunction(String username, String password) throws SQLException{
         
+        // integer var
+        int amount = -1;
+        // connection to the database
         Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/ProjectDB", "raposo","raposo0990123");
+        // prepare to create a statement
         Statement ps = con.createStatement();
+
         try{
-            ResultSet  x = ps.executeQuery("SELECT COUNT(*) as count FROM USERS WHERE username = '"+ username +"' and password = '"+ password +"'");
+            // check if there's any user with that properties
+            ResultSet x = ps.executeQuery("SELECT COUNT(*) as count FROM USERS WHERE username = '"+ username +"' and password = '"+ password +"'");
+            // returns in 'list' form
             while (x.next()) {
-                int amount = x.getInt("count"); 
-                con.close();
-                ps.close();
-                if(amount == 1){
-                    return "home";
-                }
+                // check the int value
+                amount = x.getInt("count");
             }
-        }catch(SQLException  e){
-            con.close();
-            ps.close();
-        };
+        }catch(SQLException  e){};
+
+        // close the connection and the statement
+        con.close();
+        ps.close();
+
+        // **if there's** a user direct to the new page "home.xhtml" *else* nothing happens
+        if(amount == 1)
+            return "home";
         return "";
+
     }
-    
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    /*
+     * Goal            : Valide the sign up
+     * Parameters      : Username, Password, Password2 from the input fields (index.xhtml)
+     * Security        : If there's any user with the specific username and the passwords match, cipher the password ?? [To be approved]
+     * Dificulty Level : [0-10] --> 1
+     */
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------
     public String signUpFunction(String username, String pw, String pw2) throws SQLException{
         
+        // if the passwords are not the same
+        if(!pw.equals(pw2))
+            return "";
+
+        // integer var
+        int flag = 0;
+        // connection to the database
         Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/ProjectDB", "raposo","raposo0990123");
+        // prepare to create a statement
         Statement ps = con.createStatement();
         
-        if(!pw.equals(pw2)){
-            ps.close();
-            con.close();
-            return "";            
-        }   
         try{
+            // try to insert in the database (username is the primary key!)
             ps.executeUpdate("INSERT INTO USERS VALUES('"+ username +"','"+ pw +"')");
-            ps.close();
-            con.close();
-        }catch(SQLException  e){
-            ps.close();
-            con.close();
+            flag = 1;
+        }catch(SQLException  e){};
+
+        // close the connection and the statement
+        ps.close();
+        con.close();
+
+        // **if there's not** a user, direct to the new page "home.xhtml" *else* nothing happens
+        if(flag == 0)
             return "";
-        };
         return "home";
-    }   
+
+    }
+   // ---------------------------------------------------------------------------------------------------------------------------------------------------
+
 }
